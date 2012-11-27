9a798384757f6eea253e1cb8b22c0c6d6a937a58
Dr Nic Williams <drnicwilliams@gmail.com>
2012-11-27 09:58:35 -0800

    nice headers for sections

--- lib/bosh-bootstrap/cli.rb
+++ lib/bosh-bootstrap/cli.rb
@@ -7,12 +7,30 @@ module Bosh::Bootstrap
     desc "local", "Bootstrap bosh, using local server as inception VM"
     def local
       server = Commander::LocalServer.new
-      say "Skipping Stage 3: Create the Inception VM", :yellow
-      say "Stage 4: Preparing the Inception VM", :green
+
+      header "Stage 1: Choose infrastructure"
+      header "Stage 2: Configuration"
+
+      header "Skipping Stage 3: Create the Inception VM",
+        :skipping => "Running in local mode instead. This is the Inception VM. POW!"
+
+      header "Stage 4: Preparing the Inception VM"
       server.run(Bosh::Bootstrap::Stages::StagePrepareInceptionVm.new.commands) # TODO stop on failure
     end
 
     no_tasks do
+      # Display header for a new section of the bootstrapper
+      def header(title, options={})
+        say "" # golden whitespace
+        if skipping = options[:skipping]
+          say "Skipping #{title}", :yellow
+          say skipping
+        else
+          say title, :green
+        end
+        say "" # more golden whitespace
+      end
+
       def cyan; "\033[36m" end
       def clear; "\033[0m" end
       def bold; "\033[1m" end
