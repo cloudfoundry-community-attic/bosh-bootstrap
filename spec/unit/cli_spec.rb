b978c4fc16dfe4d26408606835ad25eada813ad7
Dr Nic Williams <drnicwilliams@gmail.com>
2013-02-03 09:09:04 -0800

    move ssh command specs into own file

--- spec/unit/cli_spec.rb
+++ spec/unit/cli_spec.rb
@@ -11,37 +11,6 @@ describe Bosh::Bootstrap do
     @cmd = Bosh::Bootstrap::Cli.new
   end
 
-  describe "ssh" do
-    before do
-      @cmd.settings["inception"] = {}
-      @cmd.settings["inception"]["host"] = "5.5.5.5"
-    end
-
-    describe "normal" do
-      it "launches ssh session" do
-        @cmd.should_receive(:exit)
-        @cmd.should_receive(:system).
-          with("ssh vcap@5.5.5.5")
-        @cmd.ssh
-      end
-      it "runs ssh command" do
-        @cmd.should_receive(:exit)
-        @cmd.should_receive(:system).
-          with("ssh vcap@5.5.5.5 'some command'")
-        @cmd.ssh("some command")
-      end
-    end
-
-    describe "tmux" do
-      it "launches ssh session" do
-        @cmd.should_receive(:exit)
-        @cmd.should_receive(:system).
-          with("ssh vcap@5.5.5.5 -t 'tmux attach || tmux new-session'")
-        @cmd.tmux
-      end
-    end
-  end
-
   describe "micro_bosh_stemcell_name" do
     # The +bosh_stemcells_cmd+ has an output that looks like:
     # +-----------------------------------+--------------------+
