e57d4f989999cc486b7adac2bee46617ae1da84d
Ferran Rodenas <frodenas@gmail.com>
2013-03-13 11:36:03 +0100

    Move extract_port_definition to base_provider

--- lib/bosh/providers/aws.rb
+++ lib/bosh/providers/aws.rb
@@ -123,33 +123,6 @@ class Bosh::Providers::AWS < Bosh::Providers::BaseProvider
     true
   end
 
-  # Any of the following +port_defn+ can be used:
-  # {
-  #   ssh: 22,
-  #   http: { ports: (80..82) },
-  #   mosh: { protocol: "udp", ports: (60000..60050) }
-  #   mosh: { protocol: "rdp", ports: (3398..3398), ip_range: "196.212.12.34/32" }
-  # }
-  # In this example, 
-  #  * TCP 22 will be opened for ssh from any ip_range,
-  #  * TCP ports 80, 81, 82 for http from any ip_range,
-  #  * UDP 60000 -> 60050 for mosh from any ip_range and
-  #  * TCP 3398 for RDP from ip range: 96.212.12.34/32
-  def extract_port_definition(port_defn)
-    protocol = "tcp"
-    ip_range = "0.0.0.0/0"
-    if port_defn.is_a? Integer
-      port_range = (port_defn..port_defn)
-    elsif port_defn.is_a? Range
-      port_range = port_defn
-    elsif port_defn.is_a? Hash
-      protocol = port_defn[:protocol] if port_defn[:protocol]
-      port_range = port_defn[:ports]  if port_defn[:ports]
-      ip_range = port_defn[:ip_range] if port_defn[:ip_range]
-    end
-    [protocol, port_range, ip_range]
-  end
-
   def port_open?(ip_permissions, port_range, protocol, ip_range)
     ip_permissions && ip_permissions.find do |ip| 
       ip["ipProtocol"] == protocol \
