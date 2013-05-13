e9675dc9bd4f060be6dfe42039f3b05c23d380c9
Dr Nic Williams <drnicwilliams@gmail.com>
2013-05-13 10:49:25 -0700

    sketching out an aws integration test

---
+++ spec/integration/aws/aws_basic_spec.rb
@@ -0,0 +1,17 @@
+describe "AWS deployment using gems and publish stemcells" do
+  include FileUtils
+
+  before { prepare_aws_test }
+  after { destroy_test_constructs }
+
+  xit "creates an EC2 inception/microbosh with the associated resources" do
+    create_manifest
+    cmd.deploy
+
+    # creates a server with a specific tagged name
+    # server has a 16G volume attached (plus a root volume)
+    # IP was provisioned
+    # IP was attached to server
+  end
+
+end
