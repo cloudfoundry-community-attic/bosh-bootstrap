cd94c94a4c19ef6423c19f210c2e0954bbb0c72d
Dr Nic Williams <drnicwilliams@gmail.com>
2013-05-30 11:49:17 -0700

    Fix ssh_spec

--- spec/unit/commands/ssh_spec.rb
+++ spec/unit/commands/ssh_spec.rb
@@ -13,6 +13,7 @@ describe Bosh::Bootstrap::Cli::Commands::SSH do
   it "runs ssh" do
     setting "address.ip", "1.2.3.4"
     setting "key_pair.path", "/path/to/private/key"
+    subject.should_receive(:setup_keypair)
     subject.should_receive(:sh).with("ssh -i /path/to/private/key vcap@1.2.3.4")
     subject.perform
   end
