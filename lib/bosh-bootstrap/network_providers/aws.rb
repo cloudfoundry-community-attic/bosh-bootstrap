c4e6be01aa02190d2e98500025d2498b712e92e0
Dr Nic Williams <drnicwilliams@gmail.com>
2013-09-10 15:09:53 -0700

    WIP: refactoring SGs

--- lib/bosh-bootstrap/network_providers/aws.rb
+++ lib/bosh-bootstrap/network_providers/aws.rb
@@ -7,23 +7,28 @@ module Bosh::Bootstrap::NetworkProviders
     end
 
     def perform
-      security_groups.each do |name, ports|
-        provider_client.create_security_group(name.to_s, name.to_s, ports: ports)
-      end
+      provider_client.create_security_group("dns_server", "dns_server", ports: { protocol: "udp", ports: (53..53) })
+      provider_client.create_security_group("bosh", "bosh", ports: {
+        ssh: 22,
+        nats: 4222,
+        agent: 6868,
+        blobstore: 25250,
+        director: 25555,
+        registry: 25777
+      })
+
+      # security_groups.each do |name, ports|
+      #   provider_client.create_security_group(name.to_s, name.to_s, ports: ports)
+      # end
     end
 
     protected
-    def security_groups
-      {
-        ssh: 22,
-        dns_server: { protocol: "udp", ports: (53..53) },
-        bosh_nats_server: 4222,
-        bosh_agent_https: 6868,
-        bosh_blobstore: 25250,
-        bosh_director: 25555,
-        bosh_registry: 25777
-      }
-    end
+    # def security_groups
+    #   {
+    #     dns_server: { protocol: "udp", ports: (53..53) },
+    #     bosh: 
+    #   }
+    # end
   end
 end
 Bosh::Bootstrap::NetworkProviders.register_provider("aws", Bosh::Bootstrap::NetworkProviders::AWS)
--- spec/unit/network_spec.rb
+++ spec/unit/network_spec.rb
@@ -8,7 +8,7 @@ describe Bosh::Bootstrap::Network do
 
   it "uses NetworkProvider if available" do
     network = Bosh::Bootstrap::Network.new("aws", aws_provider_client)
-    aws_provider_client.should_receive(:create_security_group).exactly(7).times
+    aws_provider_client.should_receive(:create_security_group).exactly(2).times
     network.deploy
   end
 
