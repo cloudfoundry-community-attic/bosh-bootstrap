a337db3b0826ce11596037080ad528c6529d0d46
Jesse Proudman <jproudman@bluebox.net>
2013-09-10 16:11:32 -0700

    Include configuration option for SSL enabled endpoints.

---
+++ conf/initializer.rb
@@ -0,0 +1,11 @@
+# Used for specific modifiers for Excon / SSL settings
+require "excon"
+
+#ENV['SSL_CERT_DIR'] = 'path_to_certs'
+#ENV['SSL_CERT_FILE'] = 'path_to_file'
+#Excon.defaults[:ssl_ca_path] = ENV['SSL_CERT_DIR']
+#Excon.defaults[:ssl_ca_file] = ENV['SSL_CERT_FILE']
+
+# or
+
+Excon.defaults[:ssl_verify_peer] = false
