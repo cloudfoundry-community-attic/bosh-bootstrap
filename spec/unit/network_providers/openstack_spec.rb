aa8e8a350b11ae2279f58770ab352bb609bcc0ef
Dr Nic Williams <drnicwilliams@gmail.com>
2014-02-17 12:56:20 -0800

    Fix specs

--- spec/unit/network_providers/openstack_spec.rb
+++ spec/unit/network_providers/openstack_spec.rb
@@ -14,12 +14,12 @@ describe Bosh::Bootstrap::NetworkProviders::OpenStack do
 
   it "creates security groups it needs" do
     expected_groups = [
-      ["ssh", "ssh", ports: 22],
-      ["dns_server", "dns_server", ports: { protocol: "udp", ports: (53..53) }],
-      ["bosh", "bosh", ports: [4222, 6868, 25250, 25555, 25777]]
+      ["ssh", "ssh", 22],
+      ["dns_server", "dns_server", { protocol: "udp", ports: (53..53) }],
+      ["bosh", "bosh", [4222, 6868, 25250, 25555, 25777]]
     ]
     expected_groups.each do |security_group_name, description, ports|
-      provider_client.stub(:create_security_group).with(security_group_name, description, ports)
+      expect(provider_client).to receive(:create_security_group).with(security_group_name, description, ports)
     end
     subject.perform
   end
