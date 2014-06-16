a49fc56b6db072af50b7a7976483e0c7bb5193cb
Dr Nic Williams <drnicwilliams@gmail.com>
2014-06-16 09:58:30 -0700

    settings.provider.options.boot_from_volume enables openstack's boot_from_volume option

--- spec/unit/microbosh_providers/openstack_spec.rb
+++ spec/unit/microbosh_providers/openstack_spec.rb
@@ -70,5 +70,28 @@ describe Bosh::Bootstrap::MicroboshProviders::OpenStack do
       File.should be_exists(microbosh_yml)
       yaml_files_match(microbosh_yml, spec_asset("microbosh_yml/micro_bosh.openstack.neutron_manual.yml"))
     end
+
+    it "boot from volume" do
+      setting "provider.name", "openstack"
+      setting "provider.credentials.openstack_auth_url", "http://10.0.0.2:5000/v2.0/tokens"
+      setting "provider.credentials.openstack_username", "USER"
+      setting "provider.credentials.openstack_api_key", "PASSWORD"
+      setting "provider.credentials.openstack_tenant", "TENANT"
+      setting "provider.credentials.openstack_region", "REGION"
+      setting "address.subnet_id", "7b8788eb-b49e-4424-9065-75a6b07094ea"
+      setting "address.ip", "10.10.10.3" # network.ip
+      setting "key_pair.path", "~/.microbosh/ssh/test-bosh"
+      setting "bosh.name", "test-bosh"
+      setting "bosh.salted_password", "salted_password"
+      setting "bosh.persistent_disk", 16384
+
+      setting "provider.options.boot_from_volume", true
+
+      subject = Bosh::Bootstrap::MicroboshProviders::OpenStack.new(microbosh_yml, settings)
+
+      subject.create_microbosh_yml(settings)
+      File.should be_exists(microbosh_yml)
+      yaml_files_match(microbosh_yml, spec_asset("microbosh_yml/micro_bosh.openstack.boot_from_volume.yml"))
+    end
   end
 end
