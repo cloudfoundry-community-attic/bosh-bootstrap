fac5ae7c11eaac1fbda26ca6ca803dd78fd3b2db
Dr Nic Williams <drnicwilliams@gmail.com>
2014-10-03 16:29:49 -0700

    defaulting to new m3.medium albeit with small 4G SSD

--- lib/bosh-bootstrap/microbosh_providers/aws.rb
+++ lib/bosh-bootstrap/microbosh_providers/aws.rb
@@ -29,7 +29,7 @@ module Bosh::Bootstrap::MicroboshProviders
     end
 
     def resources_cloud_properties
-      {"instance_type"=>"m1.medium"}
+      {"instance_type"=>"m3.medium"}
     end
 
     def cloud_properties
