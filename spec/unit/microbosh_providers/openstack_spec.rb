c0b9cc2e3207168e04017486bf947ef6ddb9784b
Long Nguyen <long.nguyen11288@gmail.com>
2015-04-28 14:47:51 -0400

    add support for proxy in setting.yml

--- spec/unit/microbosh_providers/openstack_spec.rb
+++ spec/unit/microbosh_providers/openstack_spec.rb
@@ -119,6 +119,30 @@ describe Bosh::Bootstrap::MicroboshProviders::OpenStack do
       yaml_files_match(microbosh_yml, spec_asset("microbosh_yml/micro_bosh.openstack.with_recursor.yml"))
     end
 
+    it "adds proxy if present" do
+      setting "provider.name", "openstack"
+      setting "provider.credentials.openstack_auth_url", "http://10.0.0.2:5000/v2.0/tokens"
+      setting "provider.credentials.openstack_username", "USER"
+      setting "provider.credentials.openstack_api_key", "PASSWORD"
+      setting "provider.credentials.openstack_tenant", "TENANT"
+      setting "provider.credentials.openstack_region", "REGION"
+      setting "address.subnet_id", "7b8788eb-b49e-4424-9065-75a6b07094ea"
+      setting "address.pool_name", "INTERNET"
+      setting "address.ip", "1.2.3.4" # network.vip
+      setting "key_pair.path", "~/.microbosh/ssh/test-bosh"
+      setting "bosh.name", "test-bosh"
+      setting "bosh.salted_password", "salted_password"
+      setting "bosh.persistent_disk", 32768
+      setting "proxy.http_proxy", "http://192.168.1.100:8080"
+      setting "proxy.https_proxy", "https://192.168.1.100:8080"
+
+      subject = Bosh::Bootstrap::MicroboshProviders::OpenStack.new(microbosh_yml, settings, fog_compute)
+
+      subject.create_microbosh_yml(settings)
+      expect(File).to be_exists(microbosh_yml)
+      yaml_files_match(microbosh_yml, spec_asset("microbosh_yml/micro_bosh.openstack.with_proxy.yml"))
+    end
+
     it "adds state_timeout if provided" do
       setting "provider.name", "openstack"
       setting "provider.credentials.openstack_auth_url", "http://10.0.0.2:5000/v2.0/tokens"
